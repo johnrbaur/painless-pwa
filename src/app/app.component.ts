8ba55b369a1fe2e4d1d648ec9628ce108a5fdf86
John Baur <john.baur@oasisdigital.com>
2019-09-18 04:20:09 +0100

    Notify on updates to application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
 
 import { ProfileService } from './profile.service';
 import { UpdateService } from './update.service';
@@ -8,12 +10,38 @@ import { UpdateService } from './update.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   user = this.profileService.user;
   online = this.updates.online;
+  updateAvailable = false;
+  destroy = new Subject();
+
 
   constructor(
     private profileService: ProfileService,
     private updates: UpdateService
-  ) { }
+  ) {
+    updates.subscribeToUpdates(this.destroy);
+    updates.available
+      .pipe(
+        takeUntil(this.destroy),
+        tap(update => console.log('update available', update))
+      )
+      .subscribe(() => (this.updateAvailable = true));
+  }
+
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.complete();
+  }
+
+  activateUpdate() {
+    this.updates.activateUpdate();
+  }
+
+  clearUpdateMessage() {
+    this.updateAvailable = false;
+  }
+
+
 }
--- src/app/update.service.ts
+++ src/app/update.service.ts
@@ -1,14 +1,38 @@
-import { Injectable } from '@angular/core';
-import { merge, fromEvent, of } from 'rxjs';
-import { mapTo } from 'rxjs/operators';
+import { Injectable, ApplicationRef } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { merge, fromEvent, of, Observable, interval, concat } from 'rxjs';
+import { mapTo, first, takeUntil, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateService {
+  available = this.swUpdate.available;
   online = merge(
     fromEvent(window, 'online').pipe(mapTo(true)),
     fromEvent(window, 'offline').pipe(mapTo(false)),
     of(navigator.onLine)
   );
+
+  constructor(private swUpdate: SwUpdate, private appRef: ApplicationRef) {}
+
+  subscribeToUpdates(unsubscribe: Observable<any>) {
+    if (this.swUpdate.isEnabled) {
+      const isStable = this.appRef.isStable.pipe(
+        first(stable => stable === true)
+      );
+      const updateInterval = interval(30 * 1000); // check every 30 seconds
+      const updateOnceAppIsStable = concat(isStable, updateInterval);
+      updateOnceAppIsStable
+        .pipe(
+          takeUntil(unsubscribe),
+          tap(() => console.log('checking for updates'))
+        )
+        .subscribe(() => this.swUpdate.checkForUpdate());
+    }
+  }
+
+  activateUpdate() {
+    this.swUpdate.activateUpdate().then(() => document.location.reload());
+  }
 }
