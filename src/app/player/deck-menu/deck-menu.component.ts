c426a5e8dc661eb9759f80fb534fcdc1ef9846e6
John Baur <john.baur@oasisdigital.com>
2019-09-18 03:37:22 +0100

    Add push notifications

--- src/app/player/deck-menu/deck-menu.component.ts
+++ src/app/player/deck-menu/deck-menu.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { DeckManagerService, Deck } from '../../deck-manager.service';
+import { NotificationsService } from '../../notifications.service';
 
 @Component({
   selector: 'fc-deck-menu',
@@ -11,7 +12,14 @@ import { DeckManagerService, Deck } from '../../deck-manager.service';
 export class DeckMenuComponent {
   decks: Observable<Deck[]>;
 
-  constructor(loader: DeckManagerService) {
+  constructor(
+    loader: DeckManagerService,
+    private notifications: NotificationsService
+  ) {
     this.decks = loader.loadAllDecks();
+  }
+
+  promptNotifications() {
+    this.notifications.subscribeToAll();
   }
 }
